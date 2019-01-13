4bfa8bcf4ff02f2ac9b68b78d5f4427ec723ebf3
DandyU <niceyuseon@gmail.com>
2019-01-13 13:19:31 +0900

    Add a library of JunitParams

--- src/test/java/me/wired/demo/event/EventTest.java
+++ src/test/java/me/wired/demo/event/EventTest.java
@@ -1,13 +1,17 @@
 package me.wired.demo.event;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
 
     @Test
@@ -37,56 +41,56 @@ public class EventTest {
     }
 
     @Test
-    public void testFree() {
+    // # 1
+    /*@Parameters({
+            "0, 0, true",
+            "100, 0, false",
+            "0, 100, false",
+    })*/
+    // # 2
+    //@Parameters(method = "paramsForTestFree")
+    // # 3
+    @Parameters
+    public void testFree(int basePrice, int maxPrice, boolean isFree) {
         // Given
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
         // When
         event.update();
         // Then
-        AssertionsForClassTypes.assertThat(event.isFree()).isTrue();
+        assertThat(event.isFree()).isEqualTo(isFree);
+    }
 
-        // Given
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-        // When
-        event.update();
-        // Then
-        AssertionsForClassTypes.assertThat(event.isFree()).isFalse();
-
-        // Given
-        event = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-        // When
-        event.update();
-        // Then
-        AssertionsForClassTypes.assertThat(event.isFree()).isFalse();
+    // convention prefix -> "parametersFor"
+    private Object[] parametersForTestFree() {
+        return new Object[] {
+                new Object[] {0, 0, true},
+                new Object[] {100, 0, false},
+                new Object[] {0, 100, false},
+                new Object[] {100, 200, false}
+        };
     }
 
     @Test
-    public void testOffline() {
+    @Parameters
+    public void testOffline(String location, boolean isOffline) {
         // Given
         Event event = Event.builder()
-                .location("마곡")
+                .location(location)
                 .build();
         // When
         event.update();
         // Then
-        AssertionsForClassTypes.assertThat(event.isOffline()).isTrue();
+        AssertionsForClassTypes.assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
 
-        // Given
-        event = Event.builder()
-                .build();
-        // When
-        event.update();
-        // Then
-        AssertionsForClassTypes.assertThat(event.isOffline()).isFalse();
-
+    private Object[] parametersForTestOffline() {
+        return new Object[] {
+                new Object[] {"마곡", true},
+                new Object[] {null, false},
+                new Object[] {"              ", false}
+        };
     }
 }
